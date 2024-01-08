d41d4788117c561d97ec6bda83349ed46ff478ea
Steve P <stevep@interrait.com>
2024-01-08 06:28:37 +0530

    Setup basic dashboard skeleton

---
+++ Views/Dashboard.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PdfSigner.Views;
+
+public partial class Dashboard : UserControl
+{
+    public Dashboard()
+    {
+        InitializeComponent();
+    }
+}
